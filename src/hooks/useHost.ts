3f6871af7be6157f939daf2850decc3fd1e34e92
Anthony Kolber <ae@aestheticallyloyal.com>
2023-08-27 17:30:01 +1000

    Get URL from lastFocusedWindow

--- src/hooks/useHost.ts
+++ src/hooks/useHost.ts
@@ -4,7 +4,10 @@ export function useHost() {
   return useQuery({
     queryKey: ['host'],
     async queryFn() {
-      const [tab] = await chrome.tabs.query({ active: true })
+      const [tab] = await chrome.tabs.query({
+        active: true,
+        lastFocusedWindow: true
+      })
       if (!tab.url) return null
       return new URL(tab.url).host
     },
