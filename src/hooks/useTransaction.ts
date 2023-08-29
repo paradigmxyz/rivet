1d82fd93c2dc72306c2514ce14483dd1268e7ef9
Harpal Jadeja <jadejaharpal14@gmail.com>
2023-08-29 12:40:07 +0530

    feat: add `useTransaction` hook

--- src/hooks/useTransaction.ts
+++ src/hooks/useTransaction.ts
@@ -1,30 +1,33 @@
-import type { Client, GetTransactionParameters, Hash } from "viem";
-import { useClient } from "./useClient";
-import { queryOptions, useQuery } from "@tanstack/react-query";
-import { createQueryKey } from "~/react-query";
+import { useClient } from './useClient'
+import { queryOptions, useQuery } from '@tanstack/react-query'
+import type { Client, GetTransactionParameters, Hash } from 'viem'
+import { createQueryKey } from '~/react-query'
 
 export const getTransactionQueryKey = createQueryKey<
   'transaction',
   [key: Client['key'], hash: Hash | (string & {}), deps: string]
 >('transaction')
 
-
-export function useTransactionQueryOptions(args: GetTransactionParameters<'latest'>) {
-    const client = useClient();
-    return queryOptions({
-        queryKey: getTransactionQueryKey([
-            client.key,
-            args.blockHash ||
-            args.blockNumber?.toString() ||
-            args.blockTag || args.hash || 'latest'
-        ]),
-        async queryFn() {
-            return (await client.getTransaction(args)) || null;
-        }
-    })
+export function useTransactionQueryOptions(
+  args: GetTransactionParameters<'latest'>,
+) {
+  const client = useClient()
+  return queryOptions({
+    queryKey: getTransactionQueryKey([
+      client.key,
+      args.blockHash ||
+        args.blockNumber?.toString() ||
+        args.blockTag ||
+        args.hash ||
+        'latest',
+    ]),
+    async queryFn() {
+      return (await client.getTransaction(args)) || null
+    },
+  })
 }
 
 export function useTransaction(args: GetTransactionParameters<'latest'>) {
-    const queryOptions = useTransactionQueryOptions(args);
-    return useQuery(queryOptions);
+  const queryOptions = useTransactionQueryOptions(args)
+  return useQuery(queryOptions)
 }
