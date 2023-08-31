e962c17f00c1ade9655d5780ec54e412c33a1438
moxey.eth <jakemoxey@gmail.com>
2023-08-31 12:00:13 +1000

    refactor

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,6 +1,12 @@
-import { Content, Provider, Root, Trigger } from '@radix-ui/react-tooltip'
+import {
+  Content,
+  Portal,
+  Provider,
+  Root,
+  Trigger,
+} from '@radix-ui/react-tooltip'
 import type { ReactNode } from 'react'
-import { Text } from '~/design-system'
+import { Box, Text } from '~/design-system'
 
 export type TooltipProps = {
   children: ReactNode
@@ -12,13 +18,26 @@ export function Tooltip({ children, label }: TooltipProps) {
     <Provider>
       <Root delayDuration={0}>
         <Trigger asChild>
-          <span tabIndex={0}>{children}</span>
+          <Box testId="haha" tabIndex={0}>
+            {children}
+          </Box>
         </Trigger>
-        <Content>
-          <Text size="12px" style={{ padding: '8px' }}>
-            {label}
-          </Text>
-        </Content>
+        <Portal>
+          <Content asChild sideOffset={4}>
+            <Box
+              backgroundColor="surface/secondary/elevated"
+              borderWidth="1px"
+              padding="6px"
+              onClick={(e) => {
+                e.stopPropagation
+                e.preventDefault()
+              }}
+              style={{ cursor: 'text', pointerEvents: 'visible' }}
+            >
+              <Text size="11px">{label}</Text>
+            </Box>
+          </Content>
+        </Portal>
       </Root>
     </Provider>
   )
