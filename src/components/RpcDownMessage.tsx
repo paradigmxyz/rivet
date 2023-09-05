479fe552f0bff7c8721c3fd3b8082dc82a4cee6a
Flocqst <flocastet@icloud.com>
2023-09-05 15:35:31 +0200

    Used Radix Dialog and cleaned styles

--- src/components/RpcDownMessage.tsx
+++ src/components/RpcDownMessage.tsx
@@ -1,31 +1,36 @@
+import * as Dialog from '@radix-ui/react-dialog'
 import { Box, Inset, Stack, Text } from '~/design-system'
 import './RpcDownMessage.css'
 
 export function RpcDown() {
   return (
-    <div className="rpcdown-overlay">
-      <Box
-        alignItems="center"
-        backgroundColor="surface/primary/elevated"
-        borderLeftWidth="1px"
-        borderRightWidth="1px"
-        borderTopWidth="1px"
-        borderBottomWidth="1px"
-        display="flex"
-        style={{
-          height: '8vh',
-          width: '80%',
-        }}
-      >
-        <Inset horizontal="8px">
-          <Stack gap="12px">
-            <Text size="12px">Anvil is disconnected.</Text>
-            <Text size="12px">
-              Once Anvil is reconnected, DevTools will automatically reconnect.
-            </Text>
-          </Stack>
-        </Inset>
-      </Box>
-    </div>
+    <Dialog.Root open>
+      <Dialog.Overlay className="rpcdown-overlay" />
+      <Dialog.Content className="rpcdown-content">
+        <Box
+          alignItems="center"
+          backgroundColor="surface/primary/elevated"
+          borderLeftWidth="1px"
+          borderRightWidth="1px"
+          borderTopWidth="1px"
+          borderBottomWidth="1px"
+          display="flex"
+          style={{
+            height: '8vh',
+            width: '80%',
+          }}
+        >
+          <Inset horizontal="8px">
+            <Stack gap="12px">
+              <Text size="12px">Anvil is disconnected.</Text>
+              <Text size="12px">
+                Once Anvil is reconnected, DevTools will automatically
+                reconnect.
+              </Text>
+            </Stack>
+          </Inset>
+        </Box>
+      </Dialog.Content>
+    </Dialog.Root>
   )
 }
