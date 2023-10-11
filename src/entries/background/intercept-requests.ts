c4ab6e4bc699ec9d45bdb710e08162871b8c94d0
moxey.eth <jakemoxey@gmail.com>
2023-10-12 09:33:03 +1100

    ci: fix prerelease

--- src/entries/background/intercept-requests.ts
+++ src/entries/background/intercept-requests.ts
@@ -11,76 +11,78 @@ export async function interceptJsonRpcRequests() {
   }: { redirectUrl: string; sessions: SessionsStore['sessions'] }) {
     const handler = (details: chrome.webRequest.WebRequestBodyDetails) => {
       ;(async () => {
-        const url = new URL(details.url).host
-        const initiator = details.initiator
-          ? new URL(details.initiator).host
-          : undefined
+        try {
+          const url = new URL(details.url).host
+          const initiator = details.initiator
+            ? new URL(details.initiator).host
+            : undefined
 
-        // Do not add intercept rule if there is no initiator.
-        if (!initiator) return
-        // Do not add intercept rule that are not JSON-RPC.
-        if (!isJsonRpcRequest(details)) return
-        // Do not add intercept rule that are coming from the redirect URL.
-        if (url === new URL(redirectUrl).host) return
-        // Do not add intercept rule that are coming from extensions.
-        if (details.initiator?.startsWith('chrome-extension://')) return
+          // Do not add intercept rule if there is no initiator.
+          if (!initiator) return
+          // Do not add intercept rule that are not JSON-RPC.
+          if (!isJsonRpcRequest(details)) return
+          // Do not add intercept rule that are coming from the redirect URL.
+          if (url === new URL(redirectUrl).host) return
+          // Do not add intercept rule that are coming from extensions.
+          if (details.initiator?.startsWith('chrome-extension://')) return
 
-        id++
+          id++
 
-        const initiatorDomain = initiator.includes('localhost')
-          ? 'localhost'
-          : initiator
+          const initiatorDomain = initiator.includes('localhost')
+            ? 'localhost'
+            : initiator
 
-        // Extract current rule from the initiator domain.
-        const rules = await chrome.declarativeNetRequest.getDynamicRules()
-        const rule = rules.find((rule) =>
-          rule.condition.initiatorDomains?.includes(initiatorDomain),
-        )
+          // Extract current rule from the initiator domain.
+          const rules = await chrome.declarativeNetRequest.getDynamicRules()
+          const rule = rules.find((rule) =>
+            rule.condition.initiatorDomains?.includes(initiatorDomain),
+          )
 
-        const initiatorAuthorized = sessions.find(
-          (x) => x.host === initiator.replace('www.', ''),
-        )
-        if (initiatorAuthorized) {
-          // If rule has already been added (same redirect url), do not add again.
-          if (rule?.action.redirect?.url === redirectUrl) return
+          const initiatorAuthorized = sessions.find(
+            (x) => x.host === initiator.replace('www.', ''),
+          )
+          if (initiatorAuthorized) {
+            // If rule has already been added (same redirect url), do not add again.
+            if (rule?.action.redirect?.url === redirectUrl) return
 
-          // Remove existing rule (that has different redirect url).
-          if (rule)
+            // Remove existing rule (that has different redirect url).
+            if (rule)
+              chrome.declarativeNetRequest.updateDynamicRules({
+                removeRuleIds: [rule?.id],
+              })
+
+            // Add intercept rule for new redirect url.
+            chrome.declarativeNetRequest.updateDynamicRules({
+              addRules: [
+                {
+                  id,
+                  priority: 1,
+                  action: {
+                    type: chrome.declarativeNetRequest.RuleActionType.REDIRECT,
+                    redirect: {
+                      url: redirectUrl,
+                    },
+                  },
+                  condition: {
+                    initiatorDomains: [initiatorDomain],
+                    urlFilter: url,
+                    resourceTypes: [
+                      chrome.declarativeNetRequest.ResourceType.XMLHTTPREQUEST,
+                    ],
+                  },
+                },
+              ],
+            })
+          } else {
+            // If rule does not exist, don't need to remove.
+            if (!rule) return
+
+            // If initiator is not authorized in the wallet session, remove intercept rule.
             chrome.declarativeNetRequest.updateDynamicRules({
               removeRuleIds: [rule?.id],
             })
-
-          // Add intercept rule for new redirect url.
-          chrome.declarativeNetRequest.updateDynamicRules({
-            addRules: [
-              {
-                id,
-                priority: 1,
-                action: {
-                  type: chrome.declarativeNetRequest.RuleActionType.REDIRECT,
-                  redirect: {
-                    url: redirectUrl,
-                  },
-                },
-                condition: {
-                  initiatorDomains: [initiatorDomain],
-                  urlFilter: url,
-                  resourceTypes: [
-                    chrome.declarativeNetRequest.ResourceType.XMLHTTPREQUEST,
-                  ],
-                },
-              },
-            ],
-          })
-        } else {
-          // If rule does not exist, don't need to remove.
-          if (!rule) return
-
-          // If initiator is not authorized in the wallet session, remove intercept rule.
-          chrome.declarativeNetRequest.updateDynamicRules({
-            removeRuleIds: [rule?.id],
-          })
-        }
+          }
+        } catch {}
       })()
     }
 
