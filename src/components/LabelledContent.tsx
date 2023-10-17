d0173445919da291055137596f8c74598773c2e9
Jimmy <jimmy.b.mau@gmail.com>
2023-10-16 21:33:19 -0400

    feat: add labelButton prop to LabelledContent

--- src/components/LabelledContent.tsx
+++ src/components/LabelledContent.tsx
@@ -8,19 +8,32 @@ import type { TextStyles } from '../design-system/components/Text.css'
 export function LabelledContent({
   children,
   label,
+  labelButton,
   labelColor = 'text/tertiary',
   width,
 }: {
   children: ReactNode
   label: string
+  labelButton?: ReactNode
   labelColor?: TextStyles['color']
   width?: StackProps['width']
 }) {
   return (
     <Stack gap="8px" width={width}>
-      <Text color={labelColor} size="9px" wrap={false}>
-        {label.toUpperCase()}
-      </Text>
+      { labelButton ? ( 
+        <Box display="flex" flexDirection="row" alignItems="flex-end" gap="2px">
+          <Text color={labelColor} size="9px" wrap={false}>
+            {label.toUpperCase()}
+          </Text>
+          <Box style={{marginBottom: "-2px"}}>
+            {labelButton}
+          </Box>
+        </Box>
+        ) : (
+        <Text color={labelColor} size="9px" wrap={false}>
+          {label.toUpperCase()}
+        </Text>
+      )}
       <Box>{children}</Box>
     </Stack>
   )
